e2c3008224ae86e641a2b7f611f5c59a9e4ceae9
Razeen Mohamed Rasheed <razeenrasheed83@gmail.com>
2024-12-26 23:17:36 +0530

    API updates

--- watchlist_api/api/urls.py
+++ watchlist_api/api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from watchlist_api.api.views import list_all_movies
+from watchlist_api.api.views import  movie_detail, create_movie ,list_movies
 
 urlpatterns = [
-    path("", list_all_movies, name="List Movies")
+    path("", list_movies, name="list_movies"),  # For GET (list all movies)
+    path("create/", create_movie, name="create_movie"),  # For POST (create a new movie)
+    path("<int:pk>/", movie_detail, name="movie_detail"),  # For GET (retrieve), PUT, and DELETE a specific movie
 ]
