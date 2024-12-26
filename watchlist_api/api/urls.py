b8af57b9b77c2983c453a0734a798a615fdac4fb
Razeen Mohamed Rasheed <razeenrasheed83@gmail.com>
2024-12-26 19:18:56 +0530

    updates

--- watchlist_api/api/urls.py
+++ watchlist_api/api/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from watchlist_api.api.views import list_all_movies
 
 urlpatterns = [
-     path("", list_all_movies,name="movies")
+    path("", list_all_movies, name="List Movies")
 ]
--- watchlist_api/api/views.py
+++ watchlist_api/api/views.py
@@ -1,14 +1,14 @@
-from watchlist_api.models import Movies
-from watchlist_api.api.serializer import MovieSerializer
+from watchlist_api.models import  Movies
+from watchlist_api.api.serializers import MovieSerializer
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
-
-@api_view()
+@api_view(['GET'])
 def list_all_movies(request):
     try:
-        movies = Movies.objects.all()
-        MovieSerializer(movies)
-        return Response(MovieSerializer.data)
+        movie = Movies.objects.all()
+        serialzed = MovieSerializer(movie,many=True)
+        return Response(serialzed.data)
     except Exception as e:
-        print(f"Error occurred: {e}")
+        print(f"Error at {e}")
+
